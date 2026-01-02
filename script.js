400f9a57e2e3edb6b39ae0c1a8ace7f526abe842
Stephania-Kamryn <kamrynstephaniar@gmail.com>
2026-01-02 16:09:06 +0530

    Fixed nebula-blur-bg

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("JS loaded");
+
 
 // ---------- MODAL FUNCTIONS ----------
 function openModal(topic) {
@@ -9,8 +9,8 @@ function openModal(topic) {
 
     if (topic === "Chandrayan-3") {
         content.innerHTML = `
-        <h2 class="text-3xl mb-4">Chandrayaan-3</h2>
-        <p class="mb-4">
+        <h2 class="text-3xl mb-4 text-white">Chandrayaan-3</h2>
+        <p class="mb-4 text-white ">
             Chandrayaan-3 is ISRO's lunar mission that achieved a successful
             soft landing near the Moon's south pole in 2023.
         </p>
@@ -21,8 +21,8 @@ function openModal(topic) {
 
     else if(topic==="JWST"){
         content.innerHTML = `
-        <h2 class="text-3xl mb-4">James Webb Space Telescope (JWST)</h2>
-        <p class="mb-4">
+        <h2 class="text-3xl mb-4 text-white">James Webb Space Telescope (JWST)</h2>
+        <p class="mb-4 text-white">
             The James Webb Space Telescope is the most powerful space telescope ever built,
             observing the universe in infrared.
         </p>
@@ -33,8 +33,8 @@ function openModal(topic) {
 
     else if(topic==="Voyager"){
         content.innerHTML = `
-        <h2 class="text-3xl mb-4">Voyager Missions</h2>
-        <p class="mb-4">
+        <h2 class="text-3xl mb-4 text-white">Voyager Missions</h2>
+        <p class="mb-4 text-white">
             The twin Voyager spacecraft have journeyed beyond our solar system,
             sending back invaluable data.
         </p>
@@ -45,8 +45,8 @@ function openModal(topic) {
 
     else if (topic === "SatelliteTracker") {
         content.innerHTML=`
-        <h2 class="text-3xl mb-4">Satellite Orbit Tracker</h2>
-        <p class="mb-4">
+        <h2 class="text-3xl mb-4 text-white">Satellite Orbit Tracker</h2>
+        <p class="mb-4 text-white">
         A Python-based satellite orbit visualization tool using Matplotlib
         It is a real-time satellite tracking system that visualizes satellite orbits using Two-Line Element (TLE) data from CelesTrak. The project fetches live satellite data, calculates orbital positions and velocities, and displays animated trajectories using Matplotlib.
         This project was built to explore how satellites move around Earth and how
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ---------- THEME TOGGLE ----------
     const toggleBtn = document.getElementById("themeToggle");
-    const body = document.body;
+    const html = document.documentElement;
 
     if (toggleBtn) {
         toggleBtn.addEventListener("click", () => {
@@ -89,7 +89,8 @@ document.addEventListener("DOMContentLoaded", () => {
             //body.classList.toggle("bg-white");
             //body.classList.toggle("text-white");
             //body.classList.toggle("text-gray-900");
-            document.documentElement.classList.toggle("dark")
+            //document.documentElement.classList.toggle("dark")
+            html.classList.toggle("dark");
         });
     }
 
